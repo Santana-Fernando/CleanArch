958241a6c4ace48d2f3e731faeebe2a607e01aec
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2023-10-18 18:23:50 -0300

    first commit

---
+++ CleanArch.Infra.Data/EntityConfigurations/ProductConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArch.Infra.Data.EntityConfigurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
+            builder.Property(p => p.Price).HasPrecision(10,2);
+
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Caderno",
+                    Description = "Caderno espiral 100 flhas",
+                    Price = 9.45m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Borracha",
+                    Description = "Borracha branca pequena",
+                    Price = 3.45m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Lápis",
+                    Description = "Lápis da Faber Castel",
+                    Price = 6.45m
+                }
+           );
+        }
+    }
+}
